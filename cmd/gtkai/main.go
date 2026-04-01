1bbd97d282dad3a54b699332d988d43286c5d94c
Jose Meira <jmeiracorbal@gmail.com>
2026-04-01 08:54:55 +0200

    chore: bump version to 0.1.4

--- cmd/gtkai/main.go
+++ cmd/gtkai/main.go
@@ -19,7 +19,7 @@ import (
 	_ "github.com/jmeiracorbal/gtk-ai/modules/ls"
 )
 
-const version = "0.1.3"
+const version = "0.1.4"
 
 func usage() {
 	fmt.Fprintf(os.Stderr, `gtkai %s — Go Token Killer
